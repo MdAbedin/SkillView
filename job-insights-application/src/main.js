ae8ef9bf6e9ccc8618c3d56fa412a5cc6a9138fe
anudeike <ikechukwuanude@gmail.com>
2020-02-29 00:06:44 -0500

    got vuhetify to work hopefully :/

--- job-insights-application/src/main.js
+++ job-insights-application/src/main.js
@@ -1,8 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css'
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import vuetify from "./plugins/vuetify";
 
 Vue.use(Vuetify);
 
@@ -10,5 +11,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
