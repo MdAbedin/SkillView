1d080abe417551719d159a21b42cac665325810c
Jasmon Cooley <jasmoncooley89@gmail.com>
2020-02-29 07:13:58 -0500

    updates

--- job-insights-application/src/components/HelloWorld.vue
+++ job-insights-application/src/components/HelloWorld.vue
@@ -72,7 +72,10 @@
                                     </span>
                                   <span class="headline font-weight-light">
                                       or drop them in here
-                                    </span>
+                                    </span>   
+                                    <div id="chart">
+                    <apexchart type="radar" height="350" :options="chartOptions" :series="series"></apexchart>
+                    </div>
                                 </v-col>
                               </v-row>
                             </v-card-text>
@@ -95,6 +98,7 @@
                   
                       </v-card>
                     </v-flex>
+                    
                   </v-layout>
                 </v-card-text>
               </v-card>
@@ -135,12 +139,14 @@
 <script>
   import ImageUpload from "../components/ImageUpload";
   import Listing from "../components/listing";
+  import VueApexCharts from 'vue-apexcharts';
 
 export default {
   name: "HelloWorld",
   components: {
+    apexchart:VueApexCharts,
     ImageUpload,
-    Listing
+    Listing,
   },
   data () {
     return {
@@ -223,7 +229,63 @@ export default {
       {type: 'Unity'},
       {type: 'MongoDB'},
       {type: 'C++'}
-    ]
+    ],
+         series: [{
+            name: 'Series 1',
+            data: [20, 100, 40, 30, 50, 80, 33],
+          }],
+          chartOptions: {
+            chart: {
+              height: 350,
+              type: 'radar',
+            },
+            dataLabels: {
+              enabled: true
+            },
+            plotOptions: {
+              radar: {
+                size: 140,
+                polygons: {
+                  strokeColor: '#e9e9e9',
+                  fill: {
+                    colors: ['#f8f8f8', '#fff']
+                  }
+                }
+              }
+            },
+            title: {
+              text: 'Radar with Polygon Fill'
+            },
+            colors: ['#FF4560'],
+            markers: {
+              size: 4,
+              colors: ['#fff'],
+              strokeColor: '#FF4560',
+              strokeWidth: 2,
+            },
+            tooltip: {
+              y: {
+                formatter: function(val) {
+                  return val
+                }
+              }
+            },
+            xaxis: {
+              categories: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+            },
+            yaxis: {
+              tickAmount: 7,
+              labels: {
+                formatter: function(val, i) {
+                  if (i % 2 === 0) {
+                    return val
+                  } else {
+                    return ''
+                  }
+                }
+              }
+            }
+          },
     }
     
   },
