b0af3738bf70c1012e72ed17fdc2af8df893a8cf
anudeike <ikechukwuanude@gmail.com>
2020-02-29 01:17:57 -0500

    sorry it took so long to get the tabs working, they're annoying

--- job-insights-application/src/App.vue
+++ job-insights-application/src/App.vue
@@ -1,36 +1,23 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <v-app-bar app>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+        <!-- keep this inplace of the logo-->
+        <v-avatar color="indigo">
+          <v-icon dark>home</v-icon>
+        </v-avatar>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <span class="ml-4 headline font-weight-light">
+          SkillView
+        </span>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn outlined color="purple">
+        Add Experience
       </v-btn>
+
     </v-app-bar>
 
     <v-content>
